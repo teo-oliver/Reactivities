f84692091bc9ce1ff9e27e3ec8764509a76d0485
Teo Oliver <teo.liz.oli@gmail.com>
2019-11-01 22:03:18 +0000

    Added Error Handling

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -4,14 +4,22 @@ import HomePage from '../../features/home/HomePage';
 import NavBar from '../../features/nav/NavBar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import ActivityForm from '../../features/activities/form/ActivityForm';
+import { ToastContainer } from 'react-toastify';
 
 import { observer } from 'mobx-react-lite';
-import { Route, withRouter, RouteComponentProps } from 'react-router-dom';
+import {
+  Route,
+  withRouter,
+  RouteComponentProps,
+  Switch
+} from 'react-router-dom';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import NotFound from './NotFound';
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
   return (
     <>
+      <ToastContainer position="bottom-right" />
       <Route exact path="/" component={HomePage} />
       <Route
         path={'/(.+)'}
@@ -19,13 +27,16 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
           <>
             <NavBar />
             <Container style={{ marginTop: '7em' }}>
-              <Route exact path="/activities" component={ActivityDashboard} />
-              <Route path="/activities/:id" component={ActivityDetails} />
-              <Route
-                key={location.key}
-                path={['/createActivity', '/manage/:id']}
-                component={ActivityForm}
-              />
+              <Switch>
+                <Route exact path="/activities" component={ActivityDashboard} />
+                <Route path="/activities/:id" component={ActivityDetails} />
+                <Route
+                  key={location.key}
+                  path={['/createActivity', '/manage/:id']}
+                  component={ActivityForm}
+                />
+                <Route component={NotFound} />
+              </Switch>
             </Container>
           </>
         )}
